4134d6797bfee2b0bf4ac7770702696ca66393a4
Pieter van Laatum <Pieter.Laatum@student.ru.nl>
2017-09-26 15:31:54 +0200

    sf

--- src/app.js
+++ src/app.js
@@ -473,12 +473,16 @@ function getNokiaMeasurements(userid) {
                                     console.log(salesforce);
                                     salesforce.sobject('Weight_Measurements__c')
                                         .create({
-                                            RecordTypeId: '0120Y0000015YRyQAM',
-                                            Account__c: client.handle,
-                                            Date_Time_Measurement__c: date,
-                                            Unit_Type__c: 'kg',
-                                            Value__c: value
-                                        });
+                                                RecordTypeId: '0120Y0000015YRyQAM',
+                                                Account__c: client.handle,
+                                                Date_Time_Measurement__c: date,
+                                                Unit_Type__c: 'kg',
+                                                Value__c: value
+                                            },
+                                            function(err, ret) {
+                                                if (err || !ret.success) { return console.error(err, ret); }
+                                                console.log(err, ret);
+                                            });
                                 })
                             }
                         }
