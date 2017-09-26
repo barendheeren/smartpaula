6f3351ab56f385b47d824de66e6ec6cd890bd8fc
Pieter van Laatum <Pieter.Laatum@student.ru.nl>
2017-09-26 15:30:12 +0200

    sf

--- src/app.js
+++ src/app.js
@@ -470,6 +470,7 @@ function getNokiaMeasurements(userid) {
                             if (type === 1) {
                                 pool.query("INSERT INTO measure_weight (client, measure_date, weight) VALUES ($1, $2, $3) ON CONFLICT (client, measure_date) DO UPDATE SET weight = excluded.weight", [user.client, date, value]);
                                 salesforce.login('apiuser@radbouddiabetes.trial', 'REshape911', () => {
+                                    console.log(salesforce);
                                     salesforce.sobject('Weight_Measurements__c')
                                         .create({
                                             RecordTypeId: '0120Y0000015YRyQAM',
