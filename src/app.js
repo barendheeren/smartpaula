40fa5224ef0fc715c087c9424a46ce8b82739ce7
Pieter van Laatum <Pieter901@hotmail.com>
2017-10-06 13:15:06 +0200

    feedback

--- src/app.js
+++ src/app.js
@@ -87,6 +87,7 @@ function handleResponse(response, sender) {
         getOrRegisterUser(sender, 'FB').then(sender => {
             if (parameters.feedback) {
                 let feedback = parameters.feedback === '👍' ? '+' : '-';
+                console.log(parameters.feedback, feedback);
                 pool.query('UPDATE log SET feedback = $1 WHERE client = $2 AND time = (select max(time) FROM log WHERE client = $1)', [feedback, sender]);
             } else {
                 logAction(sender, intent);
