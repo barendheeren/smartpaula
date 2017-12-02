5f804d48f4d6fb0301c949241c303049d42b750c
Pieter van Laatum <Pieter901@hotmail.com>
2017-12-02 17:07:29 +0100

    Vitadock

--- src/app.js
+++ src/app.js
@@ -791,7 +791,8 @@ function getVitaDockData(client) {
         pool.query('SELECT handle FROM clients WHERE id = $1 AND type = \'SF\'', [client]).then(result => {
             let handle = result.rows[0].handle;
             vitadock.getData(userOAuth.oauth_access_token, userOAuth.oauth_access_secret, Math.round(userOAuth.time), (error, data) => {
-                if (error) { console.log(error); return; }                                      
+                if (error) { console.log(error); return; }                                     
+                console.log(data)
                 for (let item of data) {                                              
                     let date = new Date(item.measurementDate);
                     salesforce.sobject('Glucose_Measurement__c').create({
