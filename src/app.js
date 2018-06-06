856214c3a6656bf897c7a10119f3fe9758878d01
Pieter@hazdevelopment.com <Pieter@hazdevelopment.com>
2018-06-06 15:06:34 +0200

    Expert messaging

--- src/app.js
+++ src/app.js
@@ -447,7 +447,7 @@ function processMessage(message, sender, callback) {
     if (!sessionIds.has(sender)) {
         sessionIds.set(sender, uuid.v1());
     }
-    pool.query('SELECT * FROM expert_conversation WHERE client = $1 AND active = true AND (created < NOW() - INTERVAL \'10 minutes\' OR updated < NOW() - INTERVAL \'10 minutes\')', [sender]).then(result => {
+    pool.query('SELECT * FROM expert_conversation WHERE client = $1 AND active = true AND (created > NOW() - INTERVAL \'10 minutes\' OR updated > NOW() - INTERVAL \'10 minutes\')', [sender]).then(result => {
         if (result.rowCount > 0) {
             let row = result.rows[0];
              salesforce.sobject('Chat__c')
@@ -1449,7 +1449,7 @@ app.post('/webhook/salesforce', (req, res) => {
                                 sendFunction = sendAlterDeskMessageFactory(handle);
                             }
                             if(sendFunction){
-                                pool.query('SELECT * FROM expert_conversation WHERE client = $1 AND created < NOW() - INTERVAL \'10 minutes\' OR updated < NOW() - INTERVAL \'10 minutes\'', [id]).then(result => {
+                                pool.query('SELECT * FROM expert_conversation WHERE client = $1 AND (created > NOW() - INTERVAL \'10 minutes\' OR updated > NOW() - INTERVAL \'10 minutes\')', [id]).then(result => {
                                     if(result.rowCount === 0) {
                                         sendFunction({text: 'Je vroeg "' + subject + '"'});
                                     } else {
